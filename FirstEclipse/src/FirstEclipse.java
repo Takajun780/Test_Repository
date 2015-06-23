6d7caf12e1040585d5859732b686999d8dccddcd
Takajun780 <Takajun780@gmail.com>
2015-06-23 23:39:23 +0900

    2015/06/23 Commit for Attempt #1.

---
+++ FirstEclipse/src/FirstEclipse.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+
+/**
+ * @author Jumpei
+ *
+ */
+public class FirstEclipse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("こんにちは世界！");
+
+	}
+
+}
